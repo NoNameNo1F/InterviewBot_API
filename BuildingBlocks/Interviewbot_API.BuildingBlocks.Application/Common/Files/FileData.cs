bf527981e8b373acff7fc1c1ccf4a9800fe9eb91
20127670 <nonameno1f@gmail.com>
2024-12-23 13:30:43 +0700

    core: adding files, urls util

---
+++ BuildingBlocks/Interviewbot_API.BuildingBlocks.Application/Common/Files/FileData.cs
@@ -0,0 +1,17 @@
+﻿namespace Interviewbot_API.BuildingBlocks.Application.Common.Files;
+
+public class FileData
+{
+    public Stream Content { get; set; }
+    public string FileName { get; set; }
+    public string ContentType { get; set; }
+
+    public string GetPathWithFileName(string basePath)
+    {
+        var uniqueFileName = Path.GetRandomFileName();
+        var uniqueFileWithoutExtension = Path.GetFileNameWithoutExtension(uniqueFileName);
+        var fileExtension = Path.GetExtension(FileName);
+
+        return $"{basePath}/{uniqueFileWithoutExtension}{fileExtension}";
+    }
+}
---
+++ BuildingBlocks/Interviewbot_API.BuildingBlocks.Application/Common/Files/UrlData.cs
@@ -0,0 +1,11 @@
+﻿namespace Interviewbot_API.BuildingBlocks.Application.Common.Files;
+
+public class UrlData
+{
+    public ICollection<string> Urls { get; }
+
+    public UrlData(ICollection<string> urls)
+    {
+        Urls = urls;
+    }
+}
